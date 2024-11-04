e8ea2ed06f165a278775b4c1443b3faf667e08c5
darrylrambi <darrylrambi@gmail.com>
2024-11-04 23:31:15 +0700

    quick photos fix

--- lib/daftar_ulasan.dart
+++ lib/daftar_ulasan.dart
@@ -12,10 +12,10 @@ class DaftarUlasan extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            ReviewCard(username: 'Darryl', rating: 5, tanggalUlasan: '12-02-2024', ulasan: 'Bagus bang', imgSource: 'lib/assets_images/Produk1.jpg',),
+            ReviewCard(username: 'Darryl', rating: 5, tanggalUlasan: '12-02-2024', ulasan: 'Bagus bang', imgSource: 'lib/assets_images/Produk1.png',),
             ReviewCard(username: 'Dimas', rating: 1, tanggalUlasan: '12-02-2024', ulasan: 'Makanannya bau amis bang', imgSource: 'lib/assets_images/Makanan1.jpg',),
-            ReviewCard(username: 'Dafa', rating: 5, tanggalUlasan: '12-02-2024', ulasan: 'Enak makanannya bang bang', imgSource: 'lib/assets_images/Makanan1.jpg',),
-            ReviewCard(username: 'Doni', rating: 5, tanggalUlasan: '12-02-2024', ulasan: 'Tasnya bagus, sesuai gambar', imgSource: 'lib/assets_images/Produk2.jpg',),
+            ReviewCard(username: 'Dafa', rating: 5, tanggalUlasan: '12-02-2024', ulasan: 'Enak makanannya bang bang', imgSource: 'lib/assets_images/Makanan3.jpg',),
+            ReviewCard(username: 'Doni', rating: 5, tanggalUlasan: '12-02-2024', ulasan: 'Tasnya bagus, sesuai gambar', imgSource: 'lib/assets_images/Produk2.png',)
           ],
         ),
       )
